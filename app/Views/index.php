07339805b1766fbd4727582fba7ff43ca282dee4
syifa123-ainnur <62318101+syifa123-ainnur@users.noreply.github.com>
2021-03-09 10:00:11 +0700

    perubahan background

--- app/Views/index.php
+++ app/Views/index.php
@@ -41,7 +41,7 @@
     <!-- ======= Header ======= -->
     <header id="header" class="fixed-top ">
         <div class="container d-flex align-items-center">
-            <a href="index.html" class="logo mr-auto"><img src="<?= base_url(); ?>/assets/img/logo-white.png" alt="" class="img-fluid"></a>
+            <a href="index.html" class="logo mr-auto"><img src="<?= base_url(); ?>/assets/img/logo.png" alt="" class="img-fluid"></a>
             <nav class="nav-menu d-none d-lg-block">
                 <ul>
                 <li class="active"><a href="<?= base_url(); ?>">Home</a></li>
