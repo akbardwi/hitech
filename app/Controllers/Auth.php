8c133983978bb863149a0e28a38603fd4f457c3d
akbardwi <akbar.dwi14@gmail.com>
2021-03-12 19:09:42 +0700

    Update Sistem Pendaftaran

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -65,9 +65,20 @@ class Auth extends BaseController{
                         $batas = strtotime(date("26-02-2022 10:00:00"));
                         $sekarang = strtotime(date("d-m-Y H:i:s"));
                         if($batas >= $sekarang){
-                            $model->tambah($sf);
-							session()->setFlashdata('success_sf', 'Terima kasih telah mendaftar Software Fair. Nantikan informasi dari kami yang akan dikirim ke email Anda.');
-							return redirect()->to(base_url()."/#sf");                       
+                            $email_smtp = \Config\Services::email();
+                            $email_smtp->setFrom("hmti@orma.dinus.ac.id", "HMTI UDINUS");
+                            $email_smtp->setTo("$email");
+                            $email_smtp->setSubject("Konfirmasi Pendaftaran Developer HI TECH 2021");
+                            $email_smtp->setMessage("<div>Halo, $nama_ketua</div><div><br /></div><div>Terimakasih telah mendaftar sebagai developer di acara Hi-technology. Untuk para tim developer diharapkan untuk bergabung kedalam whatsapp group agar mendapatkan informasi-informasi terbaru.</div><div>Berikut link whatsapp group :</div><div><br />https://chat.whatsapp.com/CtJBWZtWbwB3hLVHAZyYx2</div><div><br /></div><div>Salam, Hi-technology 2021</div>"));
+                            $kirim = $email_smtp->send();
+                            if($kirim){
+                                $model->tambah($sf);
+                                session()->setFlashdata('success_sf', 'Terima kasih telah mendaftar Software Fair. Nantikan informasi dari kami yang akan dikirim ke email Anda.');
+                                return redirect()->to(base_url()."/#sf");
+                            } else {
+                                session()->setFlashdata('error_sf', 'Pendaftaran gagal, silahkan hubungi panitia.');
+                                return redirect()->to(base_url()."/#sf");
+                            }
                         } else {
                             session()->setFlashdata('inputs_sf', $this->request->getPost());
                             session()->setFlashdata('error_sf', 'Mohon maaf, waktu pendaftaran sudah ditutup.');
@@ -133,9 +144,20 @@ class Auth extends BaseController{
                         $batas = strtotime(date("26-02-2022 10:00:00"));
                         $sekarang = strtotime(date("d-m-Y H:i:s"));
                         if($batas >= $sekarang){
-                            $model->tambah($hf);
-							session()->setFlashdata('success_hf', 'Terima kasih telah mendaftar Hardware Fair. Nantikan informasi dari kami yang akan dikirim ke email Anda.');
-							return redirect()->to(base_url()."/#hf");                       
+                            $email_smtp = \Config\Services::email();
+                            $email_smtp->setFrom("hmti@orma.dinus.ac.id", "HMTI UDINUS");
+                            $email_smtp->setTo("$email");
+                            $email_smtp->setSubject("Konfirmasi Pendaftaran Developer HI TECH 2021");
+                            $email_smtp->setMessage("<div>Halo, $nama_ketua</div><div><br /></div><div>Terimakasih telah mendaftar sebagai developer di acara Hi-technology. Untuk para tim developer diharapkan untuk bergabung kedalam whatsapp group agar mendapatkan informasi-informasi terbaru.</div><div>Berikut link whatsapp group :</div><div><br />https://chat.whatsapp.com/CtJBWZtWbwB3hLVHAZyYx2</div><div><br /></div><div>Salam, Hi-technology 2021</div>"));
+                            $kirim = $email_smtp->send();
+                            if($kirim){
+                                $model->tambah($hf);
+                                session()->setFlashdata('success_hf', 'Terima kasih telah mendaftar Hardware Fair. Nantikan informasi dari kami yang akan dikirim ke email Anda.');
+                                return redirect()->to(base_url()."/#hf");
+                            } else {
+                                session()->setFlashdata('error_hf', 'Pendaftaran gagal, silahkan hubungi panitia.');
+                                return redirect()->to(base_url()."/#hf");
+                            }                       
                         } else {
                             session()->setFlashdata('inputs_hf', $this->request->getPost());
                             session()->setFlashdata('error_hf', 'Mohon maaf, waktu pendaftaran sudah ditutup.');
@@ -207,9 +229,20 @@ class Auth extends BaseController{
                         $batas = strtotime(date("26-02-2022 10:00:00"));
                         $sekarang = strtotime(date("d-m-Y H:i:s"));
                         if($batas >= $sekarang){
-                            $model->tambah($ot);
-							session()->setFlashdata('success_ot', 'Terima kasih telah mendaftar Open Talk. Nantikan informasi dari kami yang akan dikirim ke email Anda.');
-							return redirect()->to(base_url()."/#ot");                       
+                            $email_smtp = \Config\Services::email();
+                            $email_smtp->setFrom("hmti@orma.dinus.ac.id", "HMTI UDINUS");
+                            $email_smtp->setTo("$email_ot");
+                            $email_smtp->setSubject("Konfirmasi Pendaftaran Open Talk HI TECH 2021");
+                            $email_smtp->setMessage("<div>Halo, $nama_ot</div><div><br /></div><div>Terimakasih telah mendaftar Open Talk di acara Hi-technology. Untuk para peserta diharapkan untuk bergabung kedalam whatsapp group agar mendapatkan informasi-informasi terbaru.</div><div>Berikut link whatsapp group :</div><div><br />https://chat.whatsapp.com/JrFMLbsC23pLR19QLFy2AJ</div><div><br /></div><div>Salam, Hi-technology 2021</div>"));
+                            $kirim = $email_smtp->send();
+                            if($kirim){
+                                $model->tambah($ot);
+                                session()->setFlashdata('success_ot', 'Terima kasih telah mendaftar Open Talk. Nantikan informasi dari kami yang akan dikirim ke email Anda.');
+                                return redirect()->to(base_url()."/#ot");
+                            } else {
+                                session()->setFlashdata('error_ot', 'Pendaftaran gagal, silahkan hubungi panitia.');
+                                return redirect()->to(base_url()."/#ot");
+                            }                       
                         } else {
                             session()->setFlashdata('inputs_ot', $this->request->getPost());
                             session()->setFlashdata('error_ot', 'Mohon maaf, waktu pendaftaran sudah ditutup.');
@@ -278,9 +311,10 @@ class Auth extends BaseController{
                                     'status'        => 0,
                                     'verif_code'    => $kode
                                 ];
+                                
                                 $model->tambah($data);
                                 session()->setFlashdata('success_visitor', 'Terima kasih telah mendaftar sebagai pengunjung. Nantikan informasi dari kami yang akan dikirim ke email Anda.');
-                                return redirect()->to(base_url()."/#pengunjung");   
+                                return redirect()->to(base_url()."/#pengunjung");
                             } else {
                                 session()->setFlashdata('error_visitor', 'Gagal, silahkan coba lagi.');
                                 return redirect()->to(base_url()."/#pengunjung"); 
@@ -326,7 +360,7 @@ class Auth extends BaseController{
                 if($check_user){
                     if(password_verify($pass, $check_user['password'])){
                         if($check_user['status'] == 0){
-                            session()->setFlashdata('error_visitors', 'Cek email untuk verifikasi dahulu.');
+                            session()->setFlashdata('error_visitors', 'Silahkan cek inbox/spam email untuk verifikasi terlebih dahulu.');
                             return redirect()->to(base_url()."/#pengunjung");
                         } else {
                             session()->set('user_email',$check_user['email']);
@@ -358,9 +392,16 @@ class Auth extends BaseController{
         $visitor  	= $db->table('visitor');
 
         if(count($data_user) > 0){
-            $email = $data_user['email'];
+            $email  = $data_user['email'];
+            $nama   = $data_user['fullname'];
             $query = $db->query("UPDATE visitor SET status = '1', verif_code = '' WHERE email = '$email'");
             if($query){
+                $email_smtp = \Config\Services::email();
+                $email_smtp->setFrom("hmti@orma.dinus.ac.id", "HMTI UDINUS");
+                $email_smtp->setTo("$email");
+                $email_smtp->setSubject("Konfirmasi Pendaftaran Pengunjung HI TECH 2021");
+                $email_smtp->setMessage("<div>Halo, $nama</div><div><br /></div><div>Terimakasih telah mendaftar sebagai pengunjung di acara Hi-technology. Untuk para peserta diharapkan untuk bergabung kedalam whatsapp group agar mendapatkan informasi-informasi terbaru.</div><div>Berikut link whatsapp group :</div><div><br />https://chat.whatsapp.com/HH0P9ffhJd51Vmb8hoGdIZ</div><div><br /></div><div>Salam, Hi-technology 2021</div>"));
+                $kirim = $email_smtp->send();
                 session()->setFlashdata('success_visitors', 'Email berhasil diverifikasi.');
                 return redirect()->to(base_url()."/#pengunjung"); 
             } else {
