1b6575f6572bef4f6caf6788cb35df4974e1ee5e
syifa123-ainnur <62318101+syifa123-ainnur@users.noreply.github.com>
2021-04-01 22:40:38 +0700

    update kalimat index developer
    -perubahan backgorund mobile version

--- app/Views/vote/index_dev.php
+++ app/Views/vote/index_dev.php
@@ -2,12 +2,10 @@
         <h1 style="text-align: center; color: #fff">Selamat Datang, <?= $user_login['fullname']; ?></h1>
         <br>
         <p style="text-align: left; color: #fff">Ini merupakan halaman dari sistem forum interaksi acara Hi-Technology 2021. Para Developer Hardware fair & Software fair diharapkan memberikan pertanyaan pada forum  yang telah di sediakan dengan ketentuan umum sebagai berikut:</p>
-        <p style="text-align: left; color: #fff">1. Prosedur input koin pengunjung oleh developer dapat diakses pada halaman forum di
-        web sistem dan diberikan langsung setelah pengunjung bertanya kepada developer.</p>
-        <p style="text-align: left; color: #fff">2. Developer <strong>WAJIB</strong> memberikan 1 pertanyaan yang nantinya apabila jawaban dari
+        <p style="text-align: left; color: #fff">1. Developer <strong>WAJIB</strong> memberikan 1 pertanyaan yang nantinya apabila jawaban dari
         peserta benar maka Developer akan memberikan maksimal 5 koin kepada pengunjung.</p>
-        <p style="text-align: left; color: #fff">3. Developer diharapkan “FAIR” dalam pemberian koin kepada pengunjung acara.</p>
-        <p style="text-align: left; color: #fff">4. Developer wajib mematuhi peraturan yang sudah ditentukan panitia</p>
+        <p style="text-align: left; color: #fff">2. Developer diharapkan “FAIR” dalam pemberian koin kepada pengunjung acara.</p>
+        <p style="text-align: left; color: #fff">3. Developer wajib mematuhi peraturan yang sudah ditentukan panitia</p>
         </div>
         <br>
         <br>
